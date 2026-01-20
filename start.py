fc87aa2a73601e16e73b761bdb82eb3c2073e428
JUSTWILLPOWER <1152679377@qq.com>
2026-01-20 21:03:03 +0800

    增加息屏

--- start.py
+++ start.py
@@ -1,11 +1,14 @@
 from paho.mqtt import client as mqtt_client
-from secret import topic, pri_key
-import os
+from secret import pc_topic, pri_key, screen_topic
+import subprocess
+
 
 broker = 'bemfa.com'
 port = 9501
-topic = topic
+pc_topic = pc_topic
 client_id = pri_key
+lock_screen_command = '''powershell -WindowStyle Hidden -command "Add-Type -MemberDefinition '[DllImport(\\"user32.dll\\")] public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name Win32 -Namespace Windows; [Windows.Win32]::SendMessage(-1, 0x0112, 0xF170, 2)"'''
+
 
 def connect_mqtt():
     # For paho-mqtt 2.0.0, you need to add the properties parameter.
@@ -29,10 +32,16 @@ def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         data = msg.payload.decode()
         print(f"Received `{data}` from `{msg.topic}` topic")
-        if data == 'off':
-            os.system("shutdown /s /t 1")
+        if msg.topic == pc_topic:
+            if data == 'off':
+                subprocess.run("shutdown /s /t 1", shell=True, capture_output=True)
+        elif msg.topic == screen_topic:
+            if data == 'off':
+                subprocess.run(lock_screen_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    client.subscribe(topic)
+
+    client.subscribe(pc_topic)
+    client.subscribe(screen_topic)
     client.on_message = on_message
 
 
